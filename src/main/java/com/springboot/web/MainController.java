ad8bbfb63942cba871b43d22e52a6c2ae6c099a8
Mar-One1822 <79852428+Mar-One1822@users.noreply.github.com>
2021-06-01 17:41:25 +0100

    Jadid

--- src/main/java/com/springboot/web/MainController.java
+++ src/main/java/com/springboot/web/MainController.java
@@ -111,17 +111,7 @@ public class MainController {
 //					new File("confirmation.pdf"));
 //			return "redirect:confirmation";
 //			}
-<<<<<<< HEAD
-		
 
-=======
-<<<<<<< HEAD
-		
-
-=======
-	
->>>>>>> branch 'main' of https://github.com/EMI-PI-s4-grp8/ApplicationLocation.git
->>>>>>> branch 'main' of https://github.com/EMI-PI-s4-grp8/ApplicationLocation.git
 //		@RequestMapping(path = "/")
 //	    public String getOrderPage(Model model) throws IOException {
 ////	        Order order = OrderHelper.getOrder()
@@ -173,15 +163,7 @@ public class MainController {
 	                .body(bytes);
 
 	    }
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-
-=======
-	 
->>>>>>> branch 'main' of https://github.com/EMI-PI-s4-grp8/ApplicationLocation.git
->>>>>>> branch 'main' of https://github.com/EMI-PI-s4-grp8/ApplicationLocation.git
 		public String dispo(Model model,
 				
 					@RequestParam(name="page",defaultValue="0")int p,
